7abdd1e57a9328179aabaa256ddd5ad8ac039ed7
aldofusterturpin <aldoft@hotmail.com>
2020-05-16 21:34:37 +0200

    Refactor and split in two files

---
+++ main/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/gin-gonic/gin"
+	"strconv"
+	"time"
+)
+
+func defaultStartTime() time.Time {
+	return time.Now().Add(-time.Minute * 15)
+}
+
+func defaultEndTime() time.Time {
+	return time.Now()
+}
+
+func convertEpochStringToUint64(epochToConvert string, defaultEpoch int64) (epochConverted int64, err error) {
+	if epochToConvert == "" {
+		return defaultEpoch, nil
+	}
+	return strconv.ParseInt(epochToConvert, 10, 64)
+}
+
+func cloudWatchInsightsQuery(svc *cloudwatchlogs.CloudWatchLogs, startTimeEpoch int64, endTimeEpoch int64, logGroupName string, queryString string) (*cloudwatchlogs.GetQueryResultsOutput, error) {
+	startQueryInput := &cloudwatchlogs.StartQueryInput{
+		StartTime:    aws.Int64(startTimeEpoch),
+		EndTime:      aws.Int64(endTimeEpoch),
+		LogGroupName: aws.String(logGroupName),
+		QueryString:  aws.String(queryString),
+	}
+
+	startQueryOutput, err := svc.StartQuery(startQueryInput)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	queryResultsInput := &cloudwatchlogs.GetQueryResultsInput{QueryId: startQueryOutput.QueryId}
+	queryResultsOutput, err := svc.GetQueryResults(queryResultsInput)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	for *queryResultsOutput.Status == cloudwatchlogs.QueryStatusRunning || *queryResultsOutput.Status == cloudwatchlogs.QueryStatusScheduled {
+		fmt.Println("INFO: Waiting query to finish")
+		queryResultsOutput, err = svc.GetQueryResults(queryResultsInput)
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, err
+		}
+	}
+	fmt.Println(*queryResultsOutput.Status)
+	return queryResultsOutput, nil
+}
+
+func initRouter(svc *cloudwatchlogs.CloudWatchLogs) *gin.Engine {
+	router := gin.Default()
+	router.GET("api/open_xml", queryUploadedOpenXmls(svc))
+	return router
+}
+
+func main() {
+	var err error
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	svc := cloudwatchlogs.New(sess)
+
+	router := initRouter(svc)
+	if err = router.Run(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
---
+++ main/uploadedOpenXmls.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"text/template"
+)
+
+func prepareInsightsQueryParametersOfUploadedXmlsQuery(c *gin.Context) (startTimeEpoch int64, endTimeEpoch int64, queryString string, err error) {
+	startTime := c.Query("start_time")
+	startTimeEpoch, startTimeError := convertEpochStringToUint64(startTime, defaultStartTime().Unix())
+	if startTimeError != nil {
+		fmt.Println(startTimeError.Error())
+		return
+	}
+
+	endTime := c.Query("end_time")
+	endTimeEpoch, endTimeError := convertEpochStringToUint64(endTime, defaultEndTime().Unix())
+	if endTimeError != nil {
+		fmt.Println(endTimeError.Error())
+		return
+	}
+
+	productNumber := c.Query("pn")
+	serialNumber := c.Query("sn")
+
+	templateString := getQueryTemplateOfUploadedXmls(productNumber, serialNumber)
+	myTemplate, err := template.New("myTemplate").Parse(templateString)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	templateMapValues := map[string]interface{}{
+		"productNumber": productNumber,
+		"serialNumber":  serialNumber,
+	}
+	var result bytes.Buffer
+	if err := myTemplate.Execute(&result, templateMapValues); err != nil {
+		fmt.Println(err.Error())
+	}
+	queryString = result.String()
+	return startTimeEpoch, endTimeEpoch, queryString, err
+}
+
+func getQueryTemplateOfUploadedXmls(productNumber string, serialNumber string) (templateString string) {
+	if productNumber != "" && serialNumber != "" {
+		return `fields @timestamp, fields.ProductNumber, fields.SerialNumber, fields.bucket_name, fields.bucket_region, fields.key, fields.topic, fields.metadata.date
+								| filter ispresent(fields.ProductNumber) and ispresent(fields.SerialNumber) and ispresent(fields.bucket_name) and ispresent(fields.bucket_region) and ispresent(fields.key) and ispresent(fields.topic) and ispresent(fields.metadata.date) and fields.ProductNumber="{{.productNumber}}" and fields.SerialNumber="{{.serialNumber}}"
+								| sort @timestamp asc
+								| limit 10000`
+	}
+	if productNumber != "" {
+		return `fields @timestamp, fields.ProductNumber, fields.SerialNumber, fields.bucket_name, fields.bucket_region, fields.key, fields.topic, fields.metadata.date
+								| filter ispresent(fields.ProductNumber) and ispresent(fields.SerialNumber) and ispresent(fields.bucket_name) and ispresent(fields.bucket_region) and ispresent(fields.key) and ispresent(fields.topic) and ispresent(fields.metadata.date) and fields.ProductNumber="{{.productNumber}}"
+								| sort @timestamp asc
+								| limit 10000`
+	}
+
+	return `fields @timestamp, fields.ProductNumber, fields.SerialNumber, fields.bucket_name, fields.bucket_region, fields.key, fields.topic, fields.metadata.date
+								| filter ispresent(fields.ProductNumber) and ispresent(fields.SerialNumber) and ispresent(fields.bucket_name) and ispresent(fields.bucket_region) and ispresent(fields.key) and ispresent(fields.topic) and ispresent(fields.metadata.date)
+								| sort @timestamp asc
+								| limit 10000`
+}
+
+func queryUploadedOpenXmls(svc *cloudwatchlogs.CloudWatchLogs) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		startTimeEpoch, endTimeEpoch, queryString, err := prepareInsightsQueryParametersOfUploadedXmlsQuery(c)
+		if err != nil {
+			return
+		}
+		logGroupName := "/aws/lambda/AWSUpload"
+		queryResultsOutput, err := cloudWatchInsightsQuery(svc, startTimeEpoch, endTimeEpoch, logGroupName, queryString)
+		if err != nil {
+			fmt.Print(err.Error())
+			return
+		}
+		c.JSON(http.StatusOK, queryResultsOutput)
+	}
+}
